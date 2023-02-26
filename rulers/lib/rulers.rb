ba3d6538baf4e063e3bf62914e03273221f71b79
Kevin <kevinmulhern23@gmail.com>
2023-02-26 18:37:57 +0000

    Finished chapter 2

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -2,18 +2,40 @@
 
 require_relative "rulers/version"
 require_relative "rulers/array"
+require_relative "rulers/routing"
 
 module Rulers
-  class Error < StandardError; end
-
   class Application
     def call(env)
-      `echo debug > debug.txt`
-      [
-        200,
-        { "Content-Type" => "text/html" },
-        ["Hello from Ruby on Rulers!"]
-      ]
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, { "Content-Type" => "text/html" }, []]
+      end
+
+      if env["PATH_INFO"] == "/"
+        return [302, { "Content-Type" => "text/html" }, File.open('public/index.html', "r")]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      begin
+        text = controller.send(act)
+      rescue Exception => e
+        return [502, { "Content-Type" => "text/html" }, ["502 - BAD REQUEST"]]
+      end
+
+      [200, { "Content-Type" => "text/html" }, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
+
+
